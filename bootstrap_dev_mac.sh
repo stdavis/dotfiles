005f765da737607926b73589266f4783232c8dcd
Scott Davis <scottdavis@Scotts-MacBook-Pro.local>
2020-11-24 13:37:30 -0700

    feat: install zsh dependencies

--- bootstrap_dev_mac.sh
+++ bootstrap_dev_mac.sh
@@ -8,7 +8,20 @@ nvm alias default 'lts/*'
 
 # dev apps
 echo "installing developer apps via brew"
+brew install python zsh zsh-autosuggestions
 brew cask install visual-studio-code
+
+# make zsh default shell
+cp ./.zshrc ~/.zshrc
+source ~/.zshrc
+chsh -s /usr/local/bin/zsh
+
+# oh my zsh
+echo "installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# spaceship prompt
+npm install -g spaceship-prompt
 
 echo "installing fonts"
 brew tap homebrew/cask-fonts
