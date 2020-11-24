2bd4421dc38a48dee5d3665f151d8a58759b5574
Scott Davis <scottdavis@Scotts-MacBook-Pro.local>
2020-11-24 13:36:42 -0700

    feat(nvm): install lts and make it default

--- bootstrap_dev_mac.sh
+++ bootstrap_dev_mac.sh
@@ -3,6 +3,8 @@ bash ./bootstrap_mac.sh
 # nvm
 echo "installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+nvm install 'lts/*'
+nvm alias default 'lts/*'
 
 # dev apps
 echo "installing developer apps via brew"
