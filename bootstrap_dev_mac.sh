3403b638920b14f0e9b7907b9316d977e6d136a4
Scott Davis <scottdavis@Scotts-MacBook-Pro.local>
2020-11-24 11:57:19 -0700

    feat: dev fonts

--- bootstrap_dev_mac.sh
+++ bootstrap_dev_mac.sh
@@ -6,3 +6,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 # dev apps
 echo "installing developer apps via brew"
 brew cask install visual-studio-code
+
+echo "installing fonts"
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
