f6924ba3dcf66fb3ac3c86b47f1588d6aede3008
stdavis <stdavis@utah.gov>
2021-06-09 10:18:13 -0600

    fix: spaceship -> starship

--- bootstrap_dev_mac.sh
+++ bootstrap_dev_mac.sh
@@ -7,7 +7,7 @@ fnm install --lts
 
 # dev apps
 echo "installing developer apps via brew"
-brew install python zsh zsh-autosuggestions git inkscape dbeaver-community gh
+brew install python zsh zsh-autosuggestions git inkscape dbeaver-community gh starship
 brew install --cask visual-studio-code qgis adoptopenjdk
 
 # make zsh default shell
@@ -15,12 +15,11 @@ cp ./.zshrc ~/.zshrc
 source ~/.zshrc
 chsh -s /usr/local/bin/zsh
 
+# TODO: copy .config/starship.toml and any others.
+
 # oh my zsh
 echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# spaceship prompt
-npm install -g spaceship-prompt
 
 echo "installing fonts"
 brew tap homebrew/cask-fonts
