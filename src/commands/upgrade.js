f4ad0daa4c7107cd0a88911ccdabb4220f04a808
stdavis <stdavis@utah.gov>
2021-06-11 17:20:53 -0600

    feat: implement prettier

--- src/commands/upgrade.js
+++ src/commands/upgrade.js
@@ -1,15 +1,15 @@
-const {Command} = require('@oclif/command')
-const {execSync} = require('child_process')
+const { Command } = require('@oclif/command');
+const { execSync } = require('child_process');
 
 class UpgradeCommand extends Command {
   run() {
-    this.log('upgrading this project to the latest version...')
+    this.log('upgrading this project to the latest version...');
     execSync('npm install -g @stdavis/dotfiles@latest', {
       stdio: 'inherit',
-    })
+    });
   }
 }
 
-UpgradeCommand.description = 'Upgrades this project to it\'s most current version'
+UpgradeCommand.description = "Upgrades this project to it's most current version";
 
-module.exports = UpgradeCommand
+module.exports = UpgradeCommand;
