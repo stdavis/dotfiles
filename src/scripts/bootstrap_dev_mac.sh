b0b96803c88569977314d3fbfdf031f2720a112e
stdavis <stdavis@utah.gov>
2021-06-15 11:36:38 -0600

    feat: ask for sudo in dev bootstrap

--- src/scripts/bootstrap_dev_mac.sh
+++ src/scripts/bootstrap_dev_mac.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 if [ $1 = "1" ] ; then
   slim=true
 fi
