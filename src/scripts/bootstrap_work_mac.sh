534189ffd0ab7c6da3f52bc2d79892d172889519
stdavis <stdavis@utah.gov>
2023-04-24 10:25:58 -0600

    Revert "chore: docker -> podman"
    This reverts commit 6b10decbf49288c6207e2555069c4f338089e314.

--- src/scripts/bootstrap_work_mac.sh
+++ src/scripts/bootstrap_work_mac.sh
@@ -7,7 +7,7 @@ brew tap facebook/fb
 brew install idb-companion
 pip install fb-idb --user
 
-brew install --cask royal-tsx slack podman-desktop google-cloud-sdk parallels citrix-workspace
+brew install --cask royal-tsx slack docker google-cloud-sdk parallels citrix-workspace
 brew install firebase-cli
 
 # terraform (https://developer.hashicorp.com/terraform/downloads)
