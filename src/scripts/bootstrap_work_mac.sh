e57720f6db00a921a496f885bd0409059fa401ec
stdavis <stdavis@utah.gov>
2023-06-26 12:09:36 -0600

    chore: remove flipper

--- src/scripts/bootstrap_work_mac.sh
+++ src/scripts/bootstrap_work_mac.sh
@@ -1,11 +1,5 @@
 # mobile app development
 brew install fastlane
-brew install --cask flipper
-
-# idb - dependency for flipper
-brew tap facebook/fb
-brew install idb-companion
-pip install fb-idb --user
 
 brew install --cask royal-tsx slack docker google-cloud-sdk parallels citrix-workspace
 brew install firebase-cli
